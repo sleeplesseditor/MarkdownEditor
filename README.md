4be87cf4b7c593283b606cea5e095ea2b3e979de
sleeplesseditor <josh@bryson.me.uk>
2018-03-15 10:38:56 +0000

    Accounts and Header components completed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MarkdownEditor
 
-This is a further experiment in using Meteor with React, as part of Stephen Grider's course on [Udemy](https://www.udemy.com/meteor-react-tutorial).
+This is a further experiment in using Meteor with React, as part of Stephen Grider's course on [Udemy](https://www.udemy.com/meteor-react-tutorial). The app uses Meteor's Blaze system for authentication.
 
 To get the app up and running, follow these steps:
 
---
+++ client/components/Accounts.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+class Accounts extends Component {
+    componentDidMount() {
+        //Render Blaze accounts form and place in div
+        this.view = Blaze.render(Template.loginButtons, 
+            ReactDOM.findDOMNode(this.refs.container));
+    }
+
+    componentWillUnmount() {
+        //Find created forms and destroy them
+        Blaze.remove(this.view);
+    }
+
+    render() {
+        return (
+            <div ref="container">
+                
+            </div>
+        );
+    }
+}
+
+export default Accounts;
